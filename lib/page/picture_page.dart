79df3005be6404d97551c22cbe2ed1ca84f03e04
lucinhu <1543498474@qq.com>
2022-09-27 17:36:05 +0800

    test

--- lib/page/picture_page.dart
+++ lib/page/picture_page.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
-import 'package:share_plus/share_plus.dart';
-import 'package:path_provider/path_provider.dart';
 
 class PicturePage extends StatelessWidget {
   const PicturePage({super.key});
